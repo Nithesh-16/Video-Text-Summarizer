5d910d1d80bcc8243e9ffb67e0be16475f475209
Nithesh-16 <nitheshravagondu@gmail.com>
2025-05-03 14:35:27 +0530

    Fix deployment issues: disable static serving and add NLTK setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+import nltk
+
+def download_nltk_data():
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+    try:
+        nltk.data.find('corpora/stopwords')
+    except LookupError:
+        nltk.download('stopwords')
+
+if __name__ == "__main__":
+    download_nltk_data() 
